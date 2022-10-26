10238f9c10ca54bf289d6a81fc9405e34f202112
hou <chunhou574197@gmail.com>
2022-10-26 17:58:12 +0800

    drafting basic image display

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,72 @@
 #include <iostream>
 #include <array>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <getopt.h>
-
 using namespace std;
+
+sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight) {
+    float windowRatio = windowWidth / (float) windowHeight;
+    float viewRatio = view.getSize().x / (float) view.getSize().y;
+    float sizeX = 1;
+    float sizeY = 1;
+    float posX = 0;
+    float posY = 0;
+
+    bool horizontalSpacing = true;
+    if (windowRatio < viewRatio)
+        horizontalSpacing = false;
+
+    if (horizontalSpacing) {
+        sizeX = viewRatio / windowRatio;
+        posX = (1 - sizeX) / 2.f;
+    }
+
+    else {
+        sizeY = windowRatio / viewRatio;
+        posY = (1 - sizeY) / 2.f;
+    }
+
+    view.setViewport( sf::FloatRect(posX, posY, sizeX, sizeY) );
+
+    return view;
+}
+
+struct res { static int width; static int height; };
+int res::width = 800;
+int res::height = 600;
+
+void openImg(string imgPath) {
+	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+	sf::RenderWindow window(sf::VideoMode(res::width, res::height, desktop.bitsPerPixel), "Liquid");
+	sf::Texture texture;
+	texture.loadFromFile(imgPath);
+	sf::Sprite imgSource;
+	imgSource.setTexture(texture);
+	if (window.getSize().x < imgSource.getLocalBounds().width)
+		imgSource.setScale(res::width/imgSource.getLocalBounds().width, res::height/imgSource.getLocalBounds().height);
+	sf::View view;
+    	view.setSize(res::width, res::height);
+	view.setCenter( view.getSize().x / 2, view.getSize().y / 2 );
+	view = getLetterboxView( view, res::width, res::height);  
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+				break;
+			}
+			if (event.type == sf::Event::Resized) {
+				view = getLetterboxView(view, event.size.width, event.size.height);
+			};
+		};
+		window.clear();
+		window.setView(view);
+		window.draw(imgSource);
+		window.display();
+	};
+};
 
 int main(int argc, char** argv) {
 	if (argc < 2) {
@@ -25,7 +88,7 @@ int main(int argc, char** argv) {
 		};
 
 		if (argParser::getArgType(opt) == 1) {
-			cout << "reading file" << endl;
+			openImg(argv[1]);
 			break;
 		}
 
