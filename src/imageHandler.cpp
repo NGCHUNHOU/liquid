8f5869fee9e09d7ed6ab54687630f67ee4511c0a
hou <chunhou574197@gmail.com>
2022-12-20 17:16:49 +0800

    add experimental piece of code to support open multiple images

--- src/imageHandler.cpp
+++ src/imageHandler.cpp
@@ -73,8 +73,73 @@ void imageHandler::openImg(string imgPath) {
 };
 void imageHandler::openMultipleImgs(char** imgPaths, short arg_c) {
 	cout << "first image path " << imgPaths[1] << "\n" << "second image path " << imgPaths[2] << endl;
-	// rough test code
-	//for (int i = 1;i < arg_c;i++) {
-	//	imageHandler::openImg(imgPaths[i]);
-	//};
+	vector<sf::Texture> images;
+	vector<sf::Sprite> sprites;
+	imageResolution imgSize = { 800, 600 };
+	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+	sf::RenderWindow window(sf::VideoMode(imgSize.width, imgSize.height, desktop.bitsPerPixel), "Liquid");
+
+	for (int i = 1;i < 3;i++) {
+		ifstream file(imgPaths[i]);
+		if (!file.good()) {
+			cout << "failed to open the images" << endl;
+			exit(1);
+		}
+
+		sf::Texture texture;
+		sf::Sprite baseImage_copy;
+		texture.loadFromFile(imgPaths[i]);
+		baseImage_copy.setTexture(texture);
+		float scaleFactor = min((float)window.getSize().x / texture.getSize().x, (float)window.getSize().y / texture.getSize().y);
+		baseImage_copy.setScale(scaleFactor, scaleFactor);
+		baseImage_copy.setOrigin(baseImage_copy.getTexture()->getSize().x / 2.0f, baseImage_copy.getTexture()->getSize().y / 2.0f);
+		baseImage_copy.setPosition(imgSize.width / 2.0f, imgSize.height / 2.0f);
+		images.push_back(texture);
+		sprites.push_back(baseImage_copy);
+	}
+
+	sf::Sprite baseImage;
+	baseImage.setTexture(images[0]);
+	float scaleFactor = min((float)window.getSize().x / images[0].getSize().x, (float)window.getSize().y / images[0].getSize().y);
+	if (scaleFactor < 1)
+		baseImage.setScale(scaleFactor, scaleFactor);
+	baseImage.setOrigin(baseImage.getTexture()->getSize().x / 2.0f, baseImage.getTexture()->getSize().y / 2.0f);
+	baseImage.setPosition(imgSize.width / 2.0f, imgSize.height / 2.0f);
+	int imageIndex = 0;
+
+	sf::View view;
+	view.setSize(imgSize.width, imgSize.height);
+	view.setCenter(view.getSize().x / 2, view.getSize().y / 2);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+				break;
+			}
+			if (event.type == sf::Event::Resized) {
+				setLetterboxView(&view, event.size.width, event.size.height);
+			};
+			if (event.key.code == sf::Keyboard::L) {
+				view.setSize((float)window.getSize().x, (float)window.getSize().y);
+				baseImage.setScale(sprites[1].getScale());
+				baseImage.setOrigin(sprites[1].getOrigin());
+				baseImage.setPosition(sprites[1].getPosition());
+				baseImage.setTexture(images[1]);
+			};
+			if (event.key.code == sf::Keyboard::H) {
+				view.setSize((float)window.getSize().x, (float)window.getSize().y);
+				baseImage.setScale(sprites[0].getScale());
+				baseImage.setOrigin(sprites[0].getOrigin());
+				baseImage.setPosition(sprites[0].getPosition());
+				baseImage.setTexture(images[0]);
+			};
+		};
+		window.clear();
+		window.setView(view);
+		window.draw(baseImage);
+		window.display();
+	};
+
 };
