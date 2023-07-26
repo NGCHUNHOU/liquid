1d23e57fc0e111b740690b209a24c85da2e1289b
hou <chunhou574197@gmail.com>
2023-07-26 16:50:08 +0800

    git ignore build files

--- build/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.obj.d
+++
@@ -1,7 +0,0 @@
-SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.obj: \
-  C:/Users/knxuser/Desktop/cppPlayground/liquid/liquid/SFML/src/SFML/System/Clock.cpp \
-  C:/Users/knxuser/Desktop/cppPlayground/liquid/liquid/SFML/include/SFML/System/Clock.hpp \
-  C:/Users/knxuser/Desktop/cppPlayground/liquid/liquid/SFML/include/SFML/System/Export.hpp \
-  C:/Users/knxuser/Desktop/cppPlayground/liquid/liquid/SFML/include/SFML/Config.hpp \
-  C:/Users/knxuser/Desktop/cppPlayground/liquid/liquid/SFML/include/SFML/System/Time.hpp \
-  C:/Users/knxuser/Desktop/cppPlayground/liquid/liquid/SFML/src/SFML/System/Win32/ClockImpl.hpp
